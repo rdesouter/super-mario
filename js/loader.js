351706d47af081365386c93d9e2b22aa9704074e
ronald <ron.desouter@gmail.com>
2020-02-18 14:52:16 +0100

    add json before loop on background

--- js/loader.js
+++ js/loader.js
@@ -2,9 +2,15 @@
 export function loadImage(url) {
     return new Promise(resolve => {
         const img = new Image();
-        img.addEventListener('load', ()=>{
+        img.addEventListener('load', () => {
             resolve(img);
         });
         img.src = url;
-    })
+    });
+
+}
+
+export function loadLevel(name) {
+    return fetch(`/levels/${name}.json`)
+        .then(resolve => resolve.json());
 }
--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
 import SpriteSheet from './SpriteSheet.js';
-import {loadImage} from './loader.js';
+import { loadImage, loadLevel } from './loader.js';
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2])=>{
+        for (let x = x1; x < x2; x++) {
+            for (let y = y1; y < y2; y++) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        } 
+    });
+}
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 loadImage('/img/sprite.png')
-.then(image => {
-    const sprites = new SpriteSheet(image, 40, 40);
-    sprites.define('ground', 2, 0);
-    sprites.define('sky', 2, 3);
+    .then(image => {
+        const sprites = new SpriteSheet(image, 40, 40);
+        sprites.define('ground', 2, 0);
+        sprites.define('sky', 2, 3);
 
-    
-    for (let x = 0; x < 35; x++){
-        for (let y =0; y < 14; y++){
-            sprites.drawTile('sky', context, x, y);
-        }
-    }
-
-    for (let x = 0; x < 30; x++){
-        for (let y = 12; y < 14; y++){
-            sprites.drawTile('ground', context, x, y);
-        }
-    }
-
-}); 
+        loadLevel('1-1')
+            .then(level => {
+                //console.log(level);
+                drawBackground(level.backgrounds[0], context, sprites);  
+                // level.backgrounds.forEach(background =>{
+                //     console.log("background", background);
+                //     drawBackground(background, context, sprites);                
+                // });    
+            });
+    }); 
