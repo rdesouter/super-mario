609bf0d672f9c39409d2a4f5e5dbceca81969e33
ronald <ron.desouter@gmail.com>
2020-02-20 22:47:15 +0100

    before create extra class for mario

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@ import { loadLevel } from './loader.js';
 import {loadBackgroundSprite, loadMarioSprite} from './sprite.js';
 import { createBackgroundLayer } from './layer.js';
 
-
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 function createSpriteLayer(sprite, position){
     return function drawSpriteLayer(context){
-        for (let i = 0; i < 120; i++) {
-            sprite.draw('idle', context, position.x + i * 3 , position.y);
+        for (let i = 0; i < 3; i++) {
+            sprite.draw('idle', context, position.x, position.y);
         }
     };
+}
+
+class Vector {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
 }
 
 //load the images and the level json in parallel
@@ -29,18 +34,21 @@ Promise.all([
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
         comp.layers.push(backgroundLayer);
 
-        const position = {
-            x: 0,
-            y: 0,
-        };
-
+        const gravity = 0.5;
+        
+        //to much info for mari need to create some class
+        const position = new Vector(0, 440);
+        const vel = new Vector(4, -20);
+        
         const spriteLayer = createSpriteLayer(marioSprite, position);
         comp.layers.push(spriteLayer);
         function update(){
             comp.draw(context);
             marioSprite.draw('idle', context, position.x, position.y);
-            position.x += 2;
-            position.y += 2;
+            position.x += vel.x;
+            position.y += vel.y;
+            vel.y += gravity;
+            
             requestAnimationFrame(update);
         }
         update();
